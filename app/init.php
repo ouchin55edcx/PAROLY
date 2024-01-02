0ed23cd0b8988ac308c916307de071c11b2d2c24
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-01-02 12:17:41 +0100

    init fixe

--- app/init.php
+++ app/init.php
@@ -2,11 +2,9 @@
 
 require_once 'config/Connection.php';
 require_once 'models/UserDAO.php';
-<<<<<<< HEAD
 require_once 'models/ClientDAO.php';
 require_once 'models/ArtistDAO.php';
 require_once 'models/AdminDAO.php';
-=======
 require_once 'models/GenreDAO.php';
 require_once 'models/LyricsDAO.php';
 require_once 'models/ReportDAO.php';
@@ -14,6 +12,5 @@ require_once 'models/InteractionsDAO.php';
 require_once 'models/AlbumDAO.php';
 require_once 'models/MusicDAO.php';
 require_once 'models/PlaylistDAO.php';
->>>>>>> 513dabb163a73a2c5a1d01467addf2740dd1a3f5
 require_once 'core/App.php';
 require_once 'core/Controller.php';
