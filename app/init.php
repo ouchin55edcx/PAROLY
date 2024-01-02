79cf3a2494e55eeafac0b3b383f43124fe17ee19
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-01-02 11:23:13 +0100

    #

--- app/init.php
+++ app/init.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once 'config/Connection.php';
+require_once 'models/UserDAO.php';
+require_once 'models/ClientDAO.php';
+require_once 'models/ArtistDAO.php';
+require_once 'models/AdminDAO.php';
 require_once 'core/App.php';
 require_once 'core/Controller.php';
 
