54cbb2fb9c6d193f1fec16299148061d30cff2cf
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-01-03 12:10:19 +0100

    added 404 page

---
+++ app/controllers/404_controller.php
@@ -0,0 +1,12 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class Home extends Controller
+{
+    public function index(...$param)
+    {
+        $this->view('404', $param);
+    }
+}
