09ca0dfd5213ff95d050e832d5917a6461c1e132
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-01-03 15:20:10 +0100

    PlaylistMusic class added

---
+++ app/models/PlaylistMusic.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'Music.php';
+require_once 'Playlist.php';
+
+class PlaylistMusic {
+
+    private $id;
+    private Playlist $playlist;
+    private Music $music;
+    
+    public function __construct()
+    {
+        $this->music = new Music();
+        $this->playlist = new Playlist();
+    }
+
+    /**
+     * Get the value of playlist
+     */ 
+    public function getPlaylist()
+    {
+        return $this->playlist;
+    }
+
+    /**
+     * Get the value of music
+     */ 
+    public function getMusic()
+    {
+        return $this->music;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
